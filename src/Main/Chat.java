c2998a93e79be59f759559d6ff99bbc7e9a3908a
DanielrR0m0 <drdanielromo@gmail.com>
2018-10-02 17:34:32 -0700

    connect to other computer

--- src/Main/Chat.java
+++ src/Main/Chat.java
@@ -1,42 +1,100 @@
 package Main;
 
-import java.io.*;
+import javafx.concurrent.Task;
+
 
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Chat {
 
-
-    public static void main(String[] args) throws IOException {
-        int portNumber = 5050;
-
-        Socket sock = new Socket(InetAddress.getLocalHost().getHostAddress(), portNumber);
-        System.out.println(InetAddress.getLocalHost().getHostAddress());
-        // reading from keyboard (keyRead object)
-        BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-        // sending to client (pwrite object)
-        OutputStream ostream = sock.getOutputStream();
-        PrintWriter pwrite = new PrintWriter(ostream, true);
+    private static Scanner scanner = new Scanner(System.in);
+    static boolean exit= false;
+    static ArrayList<User> users;
 
 
-        // receiving from server ( receiveRead  object)
-        InputStream istream = sock.getInputStream();
-        BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+    public static void main(String args[]) throws Exception
+    {
+        users = new ArrayList<User>();
+        int portNumber = Integer.parseInt(args[0]);
+        ServerSocket sersock = new ServerSocket(portNumber);
 
-        System.out.println("Start, type and press Enter key");
 
-        String receiveMessage, sendMessage;
-        while(true)
-        {
-            sendMessage = keyRead.readLine();  // keyboard reading
-            pwrite.println(sendMessage);       // sending to server
-            pwrite.flush();                    // flush the data
-            if((receiveMessage = receiveRead.readLine()) != null) //receive from server
-            {
-                System.out.println(receiveMessage); // displaying at DOS prompt
+        Task<Void> serverTask = new Task<Void>() {
+
+            @Override
+            public void run() {
+                while(!sersock.isClosed()){
+                    try{
+                        Socket connection = sersock.accept();
+                        User user = new User(connection, sersock.getLocalPort());
+                        user.printMessage();
+                        users.add(user);
+                    }catch (Exception e){
+                        System.out.println(e);
+                    }
+
+
+                }
             }
-        }
 
+            @Override
+            protected Void call() throws Exception {
+                return  null;
+
+            }
+        };
+        Thread serverThread= new Thread(serverTask);
+        serverThread.start();
+
+        while(!exit)
+        {
+            if(scanner.hasNext()){
+                String input = scanner.nextLine();
+
+                if(input.equals("0")|| input.toLowerCase().equals("exit")){
+                    sersock.close();
+                    exit=true;
+                    break;
+                }
+                else if (input.toLowerCase().contains("ip")){
+                    try {
+                        System.out.println(InetAddress.getLocalHost().getHostAddress());
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else if(input.toLowerCase().contains("port")){
+                    System.out.println(portNumber);
+                }
+                else if(input.toLowerCase().contains("help")){
+                    System.out.println("help me Please!");
+                }
+                else if(input.toLowerCase().contains("connect")){
+                    String[] inputs=  input.toLowerCase().split("\\s+");
+
+                    if(inputs.length == 3){
+                        Socket socket = new Socket(inputs[1], Integer.parseInt(inputs[2]));
+                        User user = new User(socket,Integer.parseInt(inputs[2]));
+                        users.add(user);
+                        user.sendMessage("sending connection from home"); //sending message
+                        System.out.println("connect to "+ inputs[1] + " "+ inputs[2]);
+                    }
+
+                }
+                else if (input.toLowerCase().contains("list")){
+                    System.out.println(users.size());
+                    for (User user:users) {
+                        System.out.println(user);
+                    }
+                }
+
+            }
+
+        }
     }
 }
